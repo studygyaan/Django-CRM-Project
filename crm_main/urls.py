b8a0f03e68caf1ab6644fa5727638e8263829358
huzaifsayed <huzaif.sayed@gmail.com>
2019-10-23 03:02:18 +0530

    Integrate Bootstrap 4 Template in Django

--- crm_main/urls.py
+++ crm_main/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', TemplateView.as_view(template_name='example.html')),
 ]
